88472a8a89dd7e5cc3affed6aae724f292f8ec47
Ubuntu <azureuser@bigboy.mvc5ne1vg4nullqk4dy0ximxce.dx.internal.cloudapp.net>
2024-08-04 14:43:22 +0000

    writing results

--- cp-test-python-helloworld/src/test-app.py
+++ cp-test-python-helloworld/src/test-app.py
@@ -1,14 +1,19 @@
 import sys
 import os
 import time
+import json
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from operations_api import operations
 
+result = {}
 while True:
     i = input("Enter to operate:")
     if i.isdigit():
         response = operations.request_operation(int(i))
+        result[int(i)]=int(i)**2
     else:
-        print("Invalid input")
+        with open("results.json","w") as f:
+            f.write(str(result))
+            
