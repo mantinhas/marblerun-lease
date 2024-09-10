4da66a527670052fd18a548f50675e36caa4e324
mantinhas <mantinhas@github.com>
2024-09-10 17:54:42 +0100

    added better benchmark

--- cp-test-python-helloworld/src/op-version.py
+++ cp-test-python-helloworld/src/op-version.py
@@ -23,13 +23,77 @@ def run_model(model, arg):
     # Inference
     result = model(arg)
     # Results
-    result.print()
+    # result.print()
 
+imgs=[ "https://github.com/dharmx/walls/blob/main/nature/a_beach_with_waves_and_cliffs.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_beach_with_waves_and_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_body_of_water_with_a_lit_up_tower_in_the_middle.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_building_in_the_woods.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_bush.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_fern.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_flower.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_flower_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_plant.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_plant.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_leaves_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_leaves_02.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_moss_on_a_branch.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_pink_flowers.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_red_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_deer_with_antlers_grazing_on_grass.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_fog_over_water_with_a_black_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_foggy_landscape_with_trees_and_grass.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_forest_of_trees_with_fog.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_forest_with_moss_and_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_bamboo_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_bamboo_trees_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_black_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_dark_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_fish_swimming_in_a_pond.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_green_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_pink_tulips.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_trees_with_green_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_white_flowers.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_house_in_the_woods.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_lake_surrounded_by_trees.png?raw=true",
+        "https://github.com/dharmx/walls/blob/main/nature/a_lake_with_snow_covered_mountains_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_lake_with_trees_and_clouds.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_large_rocks_in_a_desert.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_log_in_a_body_of_water_surrounded_by_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_mountain_range_with_snow_on_top.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_pile_of_logs_in_a_snowy_field.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_pile_of_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_pond_with_lily_pads_and_a_wooden_fence.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_between_rocks_with_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_running_through_a_forest.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_running_through_a_forest_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_running_through_a_rocky_canyon.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_road_with_lights_on_the_side_of_a_body_of_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_road_with_trees_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_rocky_beach_with_water_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_rocky_shore_with_waves_crashing_on_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_sand_dunes_in_the_desert.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_small_plant_in_a_pot.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_snowy_mountain_with_a_body_of_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_stairs_leading_to_a_rocky_canyon_with_Flume_Gorge_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_stone_stairs_in_a_forest.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_stream_in_the_woods.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_tall_mountain_with_clouds.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_train_tracks_in_a_tunnel.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_tree_with_pink_flowers.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_waterfall_over_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_wooden_stairs_in_a_forest.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/plants_leaves_in_the_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/rocks_in_the_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/train_tracks_in_a_forest.jpg?raw=true", 
+        "https://github.com/dharmx/walls/blob/main/nature/waves_crashing_waves_on_rocks.jpg?raw=true", "https://www.nbc.com/sites/nbcblog/files/2022/07/the-office-how-to-watch.jpg" ]
+
+
+def benchmark2(model, nimages, nloops):
+    assert isinstance(nimages, int), "Number of images must be an integer"
+    assert(nimages<=len(imgs)), f"Number of images 'nimages' must be smaller than available set totalling {len(imgs)}"
+    batch_sizes = (1,2,4,8,16,32)
+
+    request_data = []
+    model_data = []
+    total_data = []
+
+    for batch_size in batch_sizes:
+        for i in range(nloops):
+            start_time = time.perf_counter()
+            
+            for batch in range(nimages//batch_size):
+                
+                start_request_time = time.perf_counter()
+                response = operations.request_operation(batch_size)
+                end_request_time = time.perf_counter()
+                request_data.append({
+                    "batch_size" : batch_size,
+                    "experiment" : i,
+                    "duration" : end_request_time - start_request_time,
+                })
+
+                for j in range(batch_size*batch, batch_size*(batch+1), 1):
+                    logger.info("Batch_size: %d\tLoop:%d/%d\tImage: %d/%d", (batch_size, i+1, nloops, j+1, nimages))
+                    
+                    start_model_time = time.perf_counter()
+                    run_model(model,imgs[j])
+                    end_model_time = time.perf_counter()
+
+                    model_data.append(
+                        {
+                            "batch_size" : batch_size,
+                            "experiment": i,
+                            "image_id": j,
+                            "duration" : end_model_time - start_model_time,
+                        }
+                    )
+
+            end_time = time.perf_counter()
+
+            total_data.append( {
+                "environment": "epochlock_op",
+                "batch_size": batch_size,
+                "experiment": i,
+                "total_operations" : nimages,
+                "duration": end_time - start_time
+            } )
+
+    def save_as_csv(data, filename):
+        with open("results/"+filename, 'a', newline='') as csvfile:
+            fieldnames = list(data[0].keys())
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for result in data:
+                writer.writerow(result)
+
+    save_as_csv(request_data, "request_data.csv")
+    save_as_csv(model_data, "model_data.csv")
+    save_as_csv(total_data, "total_data.csv")
 
 def benchmark(model, nimages, nloops):
-    imgs=[ "https://github.com/dharmx/walls/blob/main/nature/a_beach_with_waves_and_cliffs.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_beach_with_waves_and_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_body_of_water_with_a_lit_up_tower_in_the_middle.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_building_in_the_woods.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_bush.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_fern.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_flower.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_flower_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_plant.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_a_plant.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_leaves_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_leaves_02.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_moss_on_a_branch.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_pink_flowers.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_close_up_of_red_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_deer_with_antlers_grazing_on_grass.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_fog_over_water_with_a_black_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_foggy_landscape_with_trees_and_grass.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_forest_of_trees_with_fog.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_forest_with_moss_and_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_bamboo_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_bamboo_trees_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_black_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_dark_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_fish_swimming_in_a_pond.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_green_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_pink_tulips.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_trees_with_green_leaves.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_group_of_white_flowers.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_house_in_the_woods.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_lake_surrounded_by_trees.png?raw=true",
-            "https://github.com/dharmx/walls/blob/main/nature/a_lake_with_snow_covered_mountains_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_lake_with_trees_and_clouds.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_large_rocks_in_a_desert.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_log_in_a_body_of_water_surrounded_by_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_mountain_range_with_snow_on_top.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_pile_of_logs_in_a_snowy_field.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_pile_of_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_pond_with_lily_pads_and_a_wooden_fence.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_between_rocks_with_trees.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_running_through_a_forest.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_running_through_a_forest_01.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_river_running_through_a_rocky_canyon.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_road_with_lights_on_the_side_of_a_body_of_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_road_with_trees_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_rocky_beach_with_water_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_rocky_shore_with_waves_crashing_on_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_sand_dunes_in_the_desert.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_small_plant_in_a_pot.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_snowy_mountain_with_a_body_of_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_stairs_leading_to_a_rocky_canyon_with_Flume_Gorge_in_the_background.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_stone_stairs_in_a_forest.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_stream_in_the_woods.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_tall_mountain_with_clouds.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_train_tracks_in_a_tunnel.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_tree_with_pink_flowers.png?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_waterfall_over_rocks.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/a_wooden_stairs_in_a_forest.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/plants_leaves_in_the_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/rocks_in_the_water.jpg?raw=true", "https://github.com/dharmx/walls/blob/main/nature/train_tracks_in_a_forest.jpg?raw=true", 
-            "https://github.com/dharmx/walls/blob/main/nature/waves_crashing_waves_on_rocks.jpg?raw=true", "https://www.nbc.com/sites/nbcblog/files/2022/07/the-office-how-to-watch.jpg" ]
 
     assert isinstance(nimages, int), "Number of images must be an integer"
     assert(nimages<=len(imgs)), f"Number of images 'nimages' must be smaller than available set totalling {len(imgs)}"
@@ -84,6 +148,11 @@ def benchmark(model, nimages, nloops):
             result["environment_type"] = "epochlock_op"
             all_results[batch_size].append(result)
 
+    generateResultsDataCSV(all_results, batch_sizes)
+    generateRequestDataCSV(request_data)
+    generateModelDataCSV(model_data)
+
+def generateResultsDataCSV(all_results, batch_sizes):
     with open('results/results.csv', 'w', newline='') as csvfile:
         fieldnames = ["environment_type","batch_size","elapsed_time","elapsed_time_per_op"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
@@ -109,6 +178,7 @@ def benchmark(model, nimages, nloops):
 
             writer.writerow(result)
 
+def generateRequestDataCSV(request_data):
     with open("results/request_data.csv", 'w', newline='') as csvfile:
         fieldnames = ["duration", "batch_size"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
@@ -128,6 +198,8 @@ def benchmark(model, nimages, nloops):
         writer.writerow(result)
 
 
+
+def generateModelDataCSV(model_data):
     with open("results/model_data.csv", 'w', newline='') as csvfile:
         fieldnames = ["image_id", "duration", "batch_size"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
@@ -149,6 +221,7 @@ def benchmark(model, nimages, nloops):
             }, **generateStatisticsFromData(data)}
             writer.writerow(result)
 
+
 def generateStatisticsFromData(data):
     return {
         "mean" : round(float(np.mean(data)),4),
@@ -166,7 +239,7 @@ def generateStatisticsFromData(data):
 
 def main():
     model = setup()
-    benchmark(model, 64, 5)
+    benchmark2(model, 64, 5)
 
 if __name__ == "__main__":
     main()
